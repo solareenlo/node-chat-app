b618d0ad3995d15323100b08049bd9344087791a
solareenlo <solareenlo@gmail.com>
2018-11-24 00:25:27 +0900

    Adminからのはじめの挨拶文とかをmodule化

---
+++ server/utils/message.test.js
@@ -0,0 +1,14 @@
+let expect = require('expect');
+
+let {generateMessage} = require('./message');
+
+describe('generateMessage', () => {
+  it('should generate correct message object', () => {
+    let from = 'Jen';
+    let text = 'Some message';
+    let message = generateMessage(from, text);
+
+    expect(typeof message.createdAt).toBe('number');
+    expect(message).toMatchObject({from, text});
+  });
+});
