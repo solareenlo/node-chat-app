436528654b76407643b8a84a6851be8a2e4e1683
solareenlo <solareenlo@gmail.com>
2018-11-23 22:21:27 +0900

    クライアント側からcreatMessage上で, newMessageをサーバに向けて発信

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ let socket = io();
 
 socket.on('connect', () => {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'sola',
-    text: 'Yup, that works for me.'
-  });
 });
 
 socket.on('disconnect', () => {
