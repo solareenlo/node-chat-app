f2d815abb8dd56ddd3168bad7a257f0769b03321
solareenlo <solareenlo@gmail.com>
2018-11-24 00:53:34 +0900

    新規ユーザ加入時に, 自動で他のユーザに知らせるのと, callback関数でサーバに接続できたことを新規ユーザに知らせることを実装

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,20 @@
 let socket = io();
 
-socket.on('connect', function () {
+socket.on('connect', () => {
   console.log('Connected to server');
 });
 
-socket.on('disconnect', function () {
+socket.on('disconnect', () => {
   console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function (message) {
+socket.on('newMessage', (message) => {
   console.log('newMessage', message);
+});
+
+socket.emit('createMessage', {
+  from: 'Frank',
+  text: 'Hi'
+}, (data) => {
+  console.log('Got it', data);
 });
--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ io.on('connection', (socket) => {
 
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     // io.emit('newMessage', {
     //   from: message.from,
@@ -27,6 +27,7 @@ io.on('connection', (socket) => {
     //   createdAt: new Date().getTime()
     // });
     socket.broadcast.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from server');
   });
 
   socket.on('disconnect', () => {
