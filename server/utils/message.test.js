48009367ce5c079fcb2d86585d558748976f445f
solareenlo <solareenlo@gmail.com>
2018-11-24 22:20:14 +0900

    テスト環境でgeolocationを送信できるか確認

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,7 @@
 let expect = require('expect');
 
 let {generateMessage} = require('./message');
+let {generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -10,5 +11,18 @@ describe('generateMessage', () => {
 
     expect(typeof message.createdAt).toBe('number');
     expect(message).toMatchObject({from, text});
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    let from = 'Admin';
+    let latitude = 1;
+    let longitude = 1;
+    let url = 'https://www.google.com/maps?q=1,1';
+    let location = generateLocationMessage(from, latitude, longitude);
+
+    expect(typeof location.createdAt).toBe('number');
+    expect(location).toMatchObject({from, url});
   });
 });
