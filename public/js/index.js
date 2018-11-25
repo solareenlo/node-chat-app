ff26928958e4726790077adc69daecbefc75f0f3
solareenlo <solareenlo@gmail.com>
2018-11-25 16:10:44 +0900

    momentjsを使って, 形式化された時間をchatに表示

--- public/js/index.js
+++ public/js/index.js
@@ -9,9 +9,9 @@ socket.on('disconnect', () => {
 });
 
 socket.on('newMessage', (message) => {
-  console.log('newMessage', message);
+  let formattedTime = moment(message.createdAt).format('h:mm a');
   let li = jQuery('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
   jQuery('#messages').append(li);
 });
@@ -19,8 +19,9 @@ socket.on('newMessage', (message) => {
 socket.on('newLocationMessage', (message) => {
   let li = jQuery('<li></li>');
   let a = jQuery('<a target="_blank">My current location</a>');
+  let formattedTime = moment(message.createdAt).format('h:mm a');
 
-  li.text(`${message.from}: `);
+  li.text(`${message.from} ${formattedTime}: `);
   a.attr('href', message.url);
   li.append(a);
   jQuery('#messages').append(li);
