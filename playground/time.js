20f9a6930c6dfe3e12d1709e87bab35ecae23555
solareenlo <solareenlo@gmail.com>
2018-11-25 16:09:24 +0900

    momentを使ったtimestanpsの練習

--- playground/time.js
+++ playground/time.js
@@ -10,3 +10,11 @@ date.add(1, 'year').subtract(9, 'months');
 console.log(date.format('MMM Do, YYYY'));
 console.log(date.format('YYYY年MM月DD日(ddd)HH時mm分ss秒'));
 console.log(date.format('h:mm a'));
+
+let someTimestamp = moment().valueOf();
+console.log(someTimestamp);
+console.log(moment(someTimestamp).format('h:mm a'));
+
+let createdAt = 1234;
+let date2 = moment(createdAt);
+console.log(date2.format('h:mm a'));
